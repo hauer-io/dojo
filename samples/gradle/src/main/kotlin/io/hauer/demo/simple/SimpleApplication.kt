28df2e3969c4410aa9f99ca4c2e2c71fd141c689
Jan Hauer <Jan.Hauer@EXXETA.com>
2020-09-24 17:11:06 +0200

    Adds port forward to the example

--- samples/gradle/src/main/kotlin/io/hauer/demo/simple/SimpleApplication.kt
+++ samples/gradle/src/main/kotlin/io/hauer/demo/simple/SimpleApplication.kt
@@ -2,9 +2,15 @@ package io.hauer.demo.simple
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 @SpringBootApplication
-class SimpleApplication
+class SimpleApplication {
+    @GetMapping
+    fun hello () = "Hello Workd"
+}
 
 fun main(args: Array<String>) {
     runApplication<SimpleApplication>(*args)
